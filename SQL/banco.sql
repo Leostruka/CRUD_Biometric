2af8fe01c6986c79b1e2f8f239710970a0e81545
leandro ostruka <leandro.sakae@hotmail.com>
2024-05-03 14:36:37 -0300

    .

--- SQL/banco.sql
+++ SQL/banco.sql
@@ -14,33 +14,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 -- Copiando estrutura do banco de dados para crud_users_db
 CREATE DATABASE IF NOT EXISTS `crud_users_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `crud_users_db`;
-
--- Copiando estrutura para tabela crud_users_db.auditdata
-CREATE TABLE IF NOT EXISTS `auditdata` (
-  `auditID` int DEFAULT NULL,
-  `data` int DEFAULT NULL,
-  `imageWidth` int DEFAULT NULL,
-  `imageHeight` int DEFAULT NULL,
-  KEY `FK_auditdata_fir` (`auditID`),
-  CONSTRAINT `FK_auditdata_fir` FOREIGN KEY (`auditID`) REFERENCES `fir` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Exportação de dados foi desmarcado.
-
--- Copiando estrutura para tabela crud_users_db.fir
-CREATE TABLE IF NOT EXISTS `fir` (
-  `id` int NOT NULL,
-  `hash` text NOT NULL,
-  `sample` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`,`sample`) USING BTREE,
-  CONSTRAINT `fk_userid` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Exportação de dados foi desmarcado.
 
 -- Copiando estrutura para tabela crud_users_db.user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -49,7 +25,26 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela crud_users_db.fir
+CREATE TABLE IF NOT EXISTS `fir` (
+  `id` int NOT NULL,
+  `hash` text NOT NULL,
+  `sample` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`,`sample`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando estrutura para tabela crud_users_db.auditdata
+CREATE TABLE IF NOT EXISTS `auditdata` (
+  `auditID` int DEFAULT NULL,
+  `data` int DEFAULT NULL,
+  `imageWidth` int DEFAULT NULL,
+  `imageHeight` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Adding foreign key constraints
+ALTER TABLE `fir` ADD CONSTRAINT `fk_userid` FOREIGN KEY (`id`) REFERENCES `user` (`id`);
+ALTER TABLE `auditdata` ADD KEY `FK_auditdata_fir` (`auditID`);
+ALTER TABLE `auditdata` ADD CONSTRAINT `FK_auditdata_fir` FOREIGN KEY (`auditID`) REFERENCES `fir` (`id`);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
