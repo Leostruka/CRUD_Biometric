11fb401c041e75dbd8939651daea0379ec8c80e2
leandro ostruka <leandro.sakae@hotmail.com>
2024-07-29 15:55:44 -0300

    Att Banco.sql

--- SQL/banco.sql
+++ SQL/banco.sql
@@ -5,16 +5,19 @@
 -- HeidiSQL Versão:              12.1.0.6537
 -- --------------------------------------------------------
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
 -- Copiando estrutura do banco de dados para crud_users_db
 CREATE DATABASE IF NOT EXISTS `crud_users_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `crud_users_db`;
-
--- Copiando estrutura para tabela crud_users_db.user
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL,
-  `name` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Copiando estrutura para tabela crud_users_db.auditdata
 CREATE TABLE IF NOT EXISTS `auditdata` (
@@ -22,9 +25,12 @@ CREATE TABLE IF NOT EXISTS `auditdata` (
   `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `imageWidth` int DEFAULT NULL,
   `imageHeight` int DEFAULT NULL,
+  `sample` int DEFAULT NULL,
   KEY `FK_auditdata_fir` (`id`) USING BTREE,
   CONSTRAINT `FK_auditdata_user` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
 
 -- Copiando estrutura para tabela crud_users_db.fir
 CREATE TABLE IF NOT EXISTS `fir` (
@@ -34,6 +40,17 @@ CREATE TABLE IF NOT EXISTS `fir` (
   PRIMARY KEY (`id`,`sample`) USING BTREE,
   CONSTRAINT `fk_userid` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela crud_users_db.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
